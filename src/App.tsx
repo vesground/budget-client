fb3ed7974c10490092a2008baeeaa62c755b3089
vesground <k.stas@geniusee.com>
2020-06-12 17:49:03 +0300

    WIP: setup graphql link

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,53 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-client';
+// import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 
 import './App.scss';
 
 import Routes from 'routes/index';
 import store from 'store/index';
 
-const client = new ApolloClient({ uri: process.env.GRAPHQL_API_ENDPOINT_URL });
+// Instantiate required constructor fields
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  // Provide required constructor fields
+  cache,
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: process.env.REACT_APP_GRAPHQL_API_ENDPOINT_URL,
+      // credentials: 'same-origin',
+      fetchOptions: {
+        mode: 'no-cors',
+      },
+    })
+  ]),
+
+  // Provide some optional constructor fields
+  name: 'react-web-client',
+  version: '1.3',
+  queryDeduplication: false,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
+});
 
 function App() {
   return (
--- src/services/useAuth.ts
+++ src/services/useAuth.ts
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux'
 import { useQuery } from '@apollo/react-hooks';
 
 import GET_PROFILE from 'graphql/query/getProfile';
+// import MESSAGE from 'graphql/query/message';
 
 function useAuth() {
   const { loading, error, data } = useQuery(GET_PROFILE, {
